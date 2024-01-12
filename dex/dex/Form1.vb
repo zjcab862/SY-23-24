6d2e2d32595ba511292228b5bbf6f02a555c6f5c
zane <zjcab862@learner.dvusd.org>
2024-01-12 08:11:51 -0700

    added code

--- dex/dex/Form1.vb
+++ dex/dex/Form1.vb
@@ -1,4 +1,7 @@
-﻿Public Class Form1
+﻿Imports System.IO
+
+Public Class Form1
+    Dim records(50) As String
     Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
         PictureBox1.Image = Nothing
         Field1.Text = ""
@@ -31,5 +34,14 @@
         outFile.Write(PictureBox1.ImageLocation)
         outFile.WriteLine()
         outFile.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If IO.File.Exists("data.txt") Then
+            Dim inFile As New StreamReader("data.txt")
+            records(0) = inFile.ReadLine
+            records(1) = inFile.ReadLine
+            inFile.Close()
+        End If
     End Sub
 End Class
